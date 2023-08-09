e44f8bda0cd5247f957bbf611d8017b883c32c70
Janwry Dela Cruz <jbenedicto20.jbb@gmail.com>
2023-08-09 16:21:42 +0800

    tweak: remove parenthesis of each term

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@
 // https://www.blocknative.com/glossary
 const glossary = [
     {
-        term: "Application Binary Interface (ABI)",
+        term: "Application Binary Interface",
         definition: "An ABI, or Application Binary Interface, is a set of rules defining how to interact with certain software components. In the context of Ethereum, an ABI is like a blueprint that tells software how to interact with, or how to use, a specific smart contract on the blockchain. It defines the methods and structures of the smart contract so that other applications can use it correctly."
     },
     {
@@ -15,11 +15,11 @@ const glossary = [
         definition: "A blockchain is a distributed database that is shared among the nodes of a computer network. Blockchains store a continuously growing historical ledger of information (e.g. accounts and transactions) into blocks."
     },
     {
-        term: "Decentralized Application (DApp)",
+        term: "Decentralized Application",
         definition: "A decentralized application, is an application built on a decentralized blockchain network using smart contracts and Web 3.0 javascript libraries. Examples for Dapps include web3 wallets, interest-bearing cryptocurrency savings accounts, and NFT marketplaces."
     },
     {
-        term: "Decentralized Exchange (DEX)",
+        term: "Decentralized Exchange",
         definition: "DEX is an abbreviation for Decentralized Exchange. This type of crypto exchange enables users to transact in a direct peer-to-peer manner without any intermediary."
     },
     {
@@ -35,11 +35,11 @@ const glossary = [
         definition: "Chainlink is a decentralized oracle network that provides real-world data to smart contracts on the blockchain. Oracles are services that send data to the blockchain, which can trigger smart contract executions when predefined conditions are met. However, since blockchains can't access data outside their network, they rely on oracle services like Chainlink to provide this data."
     },
     {
-        term: "Decentralized Autonomous Organization (DAO)",
+        term: "Decentralized Autonomous Organization",
         definition: "A DAO, or Decentralized Autonomous Organization, is like a virtual company or club that is run by rules encoded as computer programs called smart contracts. These rules are made and changed based on the votes of its members. The main point is that no single person is in charge, rather it's collectively run by its members, and everything happens automatically according to the programmed rules."
     },
     {
-        term: "Decentralized Finance (DeFi)",
+        term: "Decentralized Finance",
         definition: "Decentralized finance (DeFi) is a blockchain-based form of finance that does not rely on central financial intermediaries such as brokerages, exchanges, or banks to offer traditional financial instruments, and instead utilizes smart contracts on blockchains, the most common being Ethereum."
     },
     {
@@ -56,7 +56,7 @@ const glossary = [
 
     },
     {
-        term: "ERC-1155 (Multi Token Standard)",
+        term: "ERC-1155",
         definition: "ERC-1155 is a technical standard used for smart contracts on the Ethereum blockchain for implementing both fungible and non-fungible tokens (NFTs). ERC stands for Ethereum Request for Comment, and 1155 is the number that was assigned to this request."
     },
     {
@@ -68,24 +68,24 @@ const glossary = [
         definition: "A browser extension wallet that enables users to securely store and manage digital assets on the decentralized web (Web3). It serves as a gateway to interact with blockchain networks, allowing users to connect their browsers to decentralized applications (dApps) and execute transactions. With its user-friendly interface, MetaMask facilitates seamless access to the world of cryptocurrencies, non-fungible tokens (NFTs), and decentralized finance (DeFi) applications."
     },
     {
-        term: "ERC-20 (Token Standard)",
+        term: "ERC-20",
         definition: "ERC-20 is a technical standard used for smart contracts on the Ethereum blockchain for implementing tokens (fungible). ERC stands for Ethereum Request for Comment, and 20 is the number that was assigned to this request."
 
     },
     {
-        term: "ERC-4337 (Account Abstraction)",
+        term: "ERC-4337",
         definition: "ERC-4337 is an Ethereum standard that achieves account abstraction on the protocol without any consensus-layer changes. It makes it possible to transact and create contracts in a single contract account. ERC-4337 is a standard for Ethereum that aims to make user accounts work more like smart contracts, a phenomenon called \"account abstraction.\""
     },
     {
-        term: "ERC-4907 (Rental NFT)",
+        term: "ERC-4907",
         definition: "ERC-4907 is a proposed Ethereum standard that extends ERC-721 to introduce \"owner\" and \"user\" roles. It streamlines NFT rentals with an automated \"expires\" function that enforces time-limited user roles, ending usage terms automatically without a second transaction."
     },
     {
-        term: "ERC-6551 (Non-fungible Token Bound Accounts)",
+        term: "ERC-6551",
         definition: "ERC-6551 is a new token standard for non-fungible tokens (NFTs) that enhances the functionality and value of NFTs by giving them smart contract capabilities. It suggests creating a dedicated cryptocurrency wallet for each ERC-721 token, effectively transforming NFTs into Token Bound Accounts (TBAs). With smart contract capabilities, NFTs can now hold other assets, interact with smart contracts, and have their own on-chain identity."
     },
     {
-        term: "ERC-721 (Non-fungible Token)",
+        term: "ERC-721",
         definition: "ERC-721 is a technical standard used for smart contracts on the Ethereum blockchain for implementing non-fungible tokens (NFTs). ERC stands for Ethereum Request for Comment, and 721 is the number that was assigned to this request."
     },
     {
@@ -93,7 +93,7 @@ const glossary = [
         definition: "A decentralized, open-source blockchain network that was launched in 2015 by its founder, Vitalik Buterin. Ethereum is the leading smart contract-enabled blockchain in the world, and it's native token, ETH, is the 2nd largest digital asset by marketcap. Blocknative's APIs support the Ethereum blockchain and the Ethereum Goerli testnet."
     },
     {
-        term: "Ethereum Name Service (ENS)",
+        term: "Ethereum Name Service",
         definition: "The Ethereum Name Service (ENS) is a decentralized naming service that allows users to register human-readable domain names for their Ethereum addresses. ENS names can be used to send and receive transactions, interact with smart contracts, and resolve decentralized websites."
     },
     {
@@ -117,7 +117,7 @@ const glossary = [
         definition: "A metaverse is an engaging digital universe, fueled by Web3 principles. It utilizes blockchain for decentralized, real-time interaction, enabling users to own digital assets, control their data, and enjoy a boundary-defying internet experience."
     },
     {
-        term: "Non-Fungible Token (NFT)",
+        term: "Non-Fungible Token",
         definition: "A non-fungible token (NFT) is a unique digital asset that is stored on a blockchain. NFTs are used to represent real-world items like art, music, in-game items, and videos."
     },
     {
